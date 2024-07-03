39bcfb669a0c3420030702cea7646db1db4a1fe1
Noat Eden <noat.eth@gmail.com>
2024-07-04 01:07:18 +0700

    remove band assets

--- scripts/verify.js
+++ scripts/verify.js
@@ -4,9 +4,6 @@ const assert = require('assert');
 const blockchains = fs.readdirSync('./images/tokens');
 
 for (const chain of blockchains) {
-  // make sure we have the chain logo image
-  assert(fs.existsSync(`./images/chains/${chain}.png`));
-
   // ==== whe rename all token address to lowercase in tokens directory
   const files = fs.readdirSync(`./images/tokens/${chain}`);
   for (const file of files) {
